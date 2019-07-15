8f4c44dcf77d1651975d3dee2f6621b1c81fe6bb
Aleksander Ficek <37374704+aleksficek@users.noreply.github.com>
2019-07-15 17:18:43 -0400

    Update main_default.c

--- main_default.c
+++ main_default.c
@@ -7,12 +7,15 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+//Need to include context.c?
 
 // Declare TCB 
 typedef struct{
+	//Bottom of task stack (highest address)
   uint32_t *base;
+	//Temp pointer
 	uint32_t *current;
+	//Top of stack, could also be on top of pushed registers (lowest address)
 	uint32_t *stack_pointer;
 	
 	uint8_t priority;
@@ -20,6 +23,7 @@ typedef struct{
 
 tcb_t TCBS[6];
 uint8_t numTasks;
+uint8_t currTask;
 
 typedef struct Node_t{
 	uint8_t task_num;
@@ -36,50 +40,54 @@ void SysTick_Handler(void) {
 
 uint32_t storeContext(void);
 void restoreContext(uint32_t sp);
+uint8_t find_next_task();
+uint8_t remove_front_node(uint8_t priority);
+void add_node(uint8_t priority_, uint8_t taskNum);
 
 void PendSV_Handler(void) {
 	
+	//Puts current tasks' node back
+	add_node(TCBS[currTask].priority, currTask);
 	
-	// Some way of choosing next Task
+	//Finds next task
+	uint8_t next_task = find_next_task();
 	
 	
-	// Assume we start we TCB0
-	
-	// Access Registers 4 to 11 and push onto TCB1s stack
-	
-	int current_task = 0;
-	int next_task = 1;
-	
+							// Some way of choosing next Task			
+							// Assume we start we TCB0
+							// Access Registers 4 to 11 and push onto TCB1s stack
 	
 	// Just push/pop R4-R11 to/from stack and manipulate the stack pointer
 	
-	
-																																																			// LOTS OF QUESTIONS HERE, CLARIFY
-	TCBS[current_task].stack_pointer = (uint32_t *)storeContext(); // Returns value of stack pointer
-	
+	//Pushes register contents onto current task's stack and updates its stack pointer																																												// LOTS OF QUESTIONS HERE, CLARIFY
+	TCBS[currTask].stack_pointer = (uint32_t *)storeContext();
+	//Pops new task's registers content (stored on its stack) into registers
 	restoreContext((uint32_t)TCBS[next_task].stack_pointer);
+	
+	//Updates current task
+	currTask = next_task;
+
+	//Removes next task's node
+	remove_front_node(TCBS[next_task].priority);
 }
 
 //Function pointer to create task function
 typedef void(*rtosTaskFunc_t)(void *args);
 
-void task_create(rtosTaskFunc_t taskFunction, void *R0, uint8_t priority_)
+//Gets called in taks initialization and pre-emting
+void add_node(uint8_t priority_, uint8_t taskNum)
 {
-	//Protects against more than 6 tasks being created
-	if (numTasks > 5)
-		return;
-	
 	//Set priority in tcb_t array
 	TCBS[numTasks].priority = priority_;
 	
-	//Iterate down linked list, two cases
+	//Iterate down linked list
 	Node_t* currNode = schedule_array[priority_];
 
 	//Case 1: if this priority's linked list is empty, just insert)
 	if (schedule_array[priority_] == NULL)
 	{
     Node_t* newNode = (Node_t*)malloc(sizeof(Node_t));
-    (*newNode).task_num = numTasks;
+    (*newNode).task_num = taskNum;
     (*newNode).next = NULL;
 
 		schedule_array[priority_] = newNode;
@@ -95,11 +103,20 @@ void task_create(rtosTaskFunc_t taskFunction, void *R0, uint8_t priority_)
     
     //Set last node pointer to pointer of new node, and set its members
     Node_t* newNode = (Node_t*)malloc(sizeof(Node_t));
-    (*newNode).task_num = numTasks;
+    (*newNode).task_num = taskNum;
     (*newNode).next = NULL;
 
     (*currNode).next = newNode;
   }
+}
+
+void task_create(rtosTaskFunc_t taskFunction, void *R0, uint8_t priority_)
+{
+	//Protects against more than 6 tasks being created
+	if (numTasks > 5)
+		return;
+	
+	add_node(priority_, numTasks);
 	
 	//Initialize TCB members
 	TCBS[numTasks].stack_pointer = TCBS[numTasks].base - 15;
@@ -114,13 +131,59 @@ void task_create(rtosTaskFunc_t taskFunction, void *R0, uint8_t priority_)
   numTasks++;
 }
 
+//Returns task number of task removed, 0 if no ready tasks at priority, 
+//-1 if invalid priority
+uint8_t remove_front_node(uint8_t priority)
+{
+  uint8_t taskNumOfRemoved = 0;
+
+  if (priority < 0 || priority > 5)
+    return -1;
+
+  if (schedule_array[priority] == NULL)
+    return 0;
+    
+  taskNumOfRemoved = (*schedule_array[priority]).task_num;
+
+  Node_t* secondNode = (*schedule_array[priority]).next;
+  free(schedule_array[priority]);
+  schedule_array[priority] = secondNode;
+
+  numTasks--;
+
+  return (uint8_t)taskNumOfRemoved;
+}
+
+//Return -1 if no available next task
+uint8_t find_next_task()
+{
+  //Iterate down bit vector until find next available priority
+  int next_priority = 5;
+  while (schedule_array[next_priority] == NULL && next_priority >= 0) {
+    next_priority--;
+  }
+  
+  //If no available next task
+  if (next_priority == -1)
+    return -1;
+
+  //If available next task, return first task num of linked list.
+  //Because available tasks are added at back of linked list
+  //and ready tasks are removed at front of linked list,
+  //tasks of equal priority that are ready are round robined
+  return (uint8_t)(*schedule_array[next_priority]).task_num;
+}
+
 void initialization(void) {
 	// Initialize TCB base address for its stack
 	
 	// Find address of main stack (first 32 bit value at 0x0 is base address)
 	// Then go up for tcbs 54321 but substract instead (subtract by 2 kib, then 1 kib (800, then 400)
 	uint32_t **mainstack = 0x0;
+	//This used to copy over main stack to task 1 stack
 	uint32_t *mainstack_address = *mainstack;
+	//This used to remember where main stack base is
+	uint32_t *mainstack_base = *mainstack;
 	
 	TCBS[5].base = (uint32_t *)(mainstack_address - 0x0800);
 	TCBS[4].base = (uint32_t *)(mainstack_address - 0x1200);
@@ -141,18 +204,28 @@ void initialization(void) {
 	uint32_t *MSP = (uint32_t *)__get_MSP();
 	TCBS[0].current = TCBS[0].base;
 	
+	//Copy everything in main stack to task 1 stack
 	while (mainstack_address >= MSP) {
 		*TCBS[0].current = *mainstack_address;
 		TCBS[0].current--;
 		mainstack_address--;
 	}
-	__set_MSP((uint32_t)mainstack_address);
 	
-	// __get_CONTROL(__set_CONTROL)
-	__set_CONTROL(1 << 1);
+	TCBS[0].stack_pointer = TCBS[0].current + 1;
 	
+	//Set MSP to mainstack base address
+	__set_MSP((uint32_t)mainstack_base);
+	
+	//Switches control from MSP to PSP
+	__set_CONTROL(__get_CONTROL() | (1 << 1));
+	
+	//Set PSP to top of task 1 stack
 	// TCB1.current may have decremented 1 too much
-	__set_PSP(*TCBS[0].current);
+	__set_PSP((uint32_t)TCBS[0].stack_pointer);
+	
+	//Begin multithread by running task 0. Correct next task will be 
+	//determined at next pre-empt in PendSV_Handler
+	currTask = 0;
 }
 
 
